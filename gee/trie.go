8fe8c6fcb6efc6158ca67640a68d4c0a4f8da17b
Huang-Xinzhong <huangxinzhong@storswift.com>
2023-06-02 09:08:25 +0800

    fix: 存储子路由是参数设置错误

--- gee/trie.go
+++ gee/trie.go
@@ -47,7 +47,7 @@ func (n *node) insert(pattern string, parts []string, height int) {
 	part := parts[height]
 	child := n.matchChild(part)
 	if child == nil {
-		child = &node{pattern: part, isWild: part[0] == ':' || part[0] == '*'}
+		child = &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
 		n.children = append(n.children, child)
 	}
 	child.insert(pattern, parts, height+1)
